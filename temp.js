e5a443d5e445a46a2b13eaa36c75e3bcbb3dc375
Emily Bullard <emilygracebullard@gmail.com>
2016-01-20 23:39:40 -0500

    Update temp.js

--- temp.js
+++ temp.js
@@ -4,12 +4,6 @@ var indoors = true;
 
 console.log("The temperature is " + temp + " degrees.");
 
-// if (temp > 80 && !precipitation) {
-//   console.log("Time to swim!");
-// } else if (indoors){
-//   console.log("Time to swim!");
-// }
-
 if (indoors) {
   console.log("Time to swim!");
 } else if (temp > 80 && !precipitation) {
